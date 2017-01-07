2167111a0a0e0b81ad88978256afaa0062188199
HiepDz <narutomanchester@gmail.com>
2017-01-08 05:58:17 +0700

    add Movie Today(random Movies) screen

--- RMovie/RMovie/AppDelegate.swift
+++ RMovie/RMovie/AppDelegate.swift
@@ -12,13 +12,25 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    
+    var countTopFilmRest = 100
+    var topMovies = [Movie]()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        SearchManager.share.searchTopFilm { (movies) in
+            for movie in movies{
+                self.topMovies.append(movie)
+            }
+        }
+        //progressLoopMovie()
+        print(topMovies.count)
         return true
     }
 
+   
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
